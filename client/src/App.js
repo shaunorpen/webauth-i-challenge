d6582283ba207f6962cac935d1875da4bb29ea11
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-11 14:47:39 +0000

    Add login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [formValues, setFormValues] = React.useState({
+    username: "",
+    password: ""
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users App</h1>
+      <Login formValues={formValues} setFormValues={setFormValues} />
     </div>
+  );
+}
+
+function Login(props) {
+  const { username, password } = props.formValues;
+
+  const handleChange = () => e => {
+    return props.setFormValues({
+      ...props.formValues,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = () => e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", { username, password })
+      .then(res => {
+        debugger;
+        console.log(res);
+      })
+      .catch(err => {
+        debugger;
+        console.log(err.message);
+      });
+  };
+
+  return (
+    <form>
+      <label htmlFor="username">Username: </label>
+      <input
+        name="username"
+        value={username}
+        onChange={handleChange()}
+        type="text"
+      ></input>
+      <label htmlFor="password">Password: </label>
+      <input
+        name="password"
+        value={password}
+        onChange={handleChange()}
+        type="password"
+      ></input>
+      <button onClick={handleSubmit()}>Submit</button>
+    </form>
   );
 }
 
